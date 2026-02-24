1ad5756c1d6fd47807214fbf09e1b005a1f5b7c3
hibabinth <hibabinth916@gmail.com>
2026-02-24 01:05:53 -0500

    model changes

--- lib/model/models.dart
+++ lib/model/models.dart
@@ -4,6 +4,8 @@ class Product {
   final String category;
   final double price;
   final String image;
+  final String description; // ← added
+  final Rating? rating; // ← added
 
   Product({
     required this.id,
@@ -11,6 +13,8 @@ class Product {
     required this.category,
     required this.price,
     required this.image,
+    required this.description,
+    this.rating,
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
@@ -18,8 +22,25 @@ class Product {
       id: json['id'],
       title: json['title'],
       category: json['category'],
-      price: json['price'].toDouble(),
+      price: (json['price'] as num).toDouble(),
       image: json['image'],
+      description: json['description'] ?? '', // ← added
+      rating:
+          json['rating'] !=
+              null // ← added
+          ? Rating.fromJson(json['rating'])
+          : null,
     );
+  }
+}
+
+class Rating {
+  final double rate;
+  final int count;
+
+  Rating({required this.rate, required this.count});
+
+  factory Rating.fromJson(Map<String, dynamic> json) {
+    return Rating(rate: (json['rate'] as num).toDouble(), count: json['count']);
   }
 }
